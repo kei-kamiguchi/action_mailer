dd3e1122dff00290d9b25809174917bf8026dc3f
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-07-26 20:31:59 +0900

    [update]本番環境でletter_opener_webの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.production?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root 'contacts#new'
   resources :contacts
 end
