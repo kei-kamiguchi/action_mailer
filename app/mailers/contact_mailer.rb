eee0861c52606aaee4dd11c407bd5f5c85b48968
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-08-05 00:55:24 +0900

    [add]メール内容のプレビュー機能を実装

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
-  def contact_mail(contact)
-    @contact = contact
-    mail to: "kei.kamiguchi517@gmail.com", subject: "お問い合わせの確認メール"
+  def contact_mail
+    @contact = params[:contact]
+    mail to: params[:contact].email, subject: "お問い合わせの確認メール"
   end
 end
--- test/mailers/previews/contact_mailer_preview.rb
+++ test/mailers/previews/contact_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
 class ContactMailerPreview < ActionMailer::Preview
-
+  def contact_mail
+    ContactMailer.with(contact: Contact.find(2)).contact_mail
+  end
 end
