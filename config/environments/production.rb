ab59624a544199a44a713189a61b12fefd770a60
keikamiguchi <kei.kamiguchi517@gmail.com>
2021-01-28 11:09:41 +0900

    修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'mighty-hamlet-83755.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'cryptic-depths-79130.herokuapp.com' }
   config.action_mailer.delivery_method = :letter_opener_web
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
